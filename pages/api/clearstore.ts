82049e20521b29ddd2361b65f44bceb515c91b87
Anne LoVerso <anne.loverso@gmail.com>
2024-09-21 01:11:19 -0400

    add clear store function

---
+++ pages/api/clearstore.ts
@@ -0,0 +1,22 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { getStore } from "@netlify/blobs";
+import { CURRENT_WEEK } from "../../src/domain/consts";
+
+type ResponseData = {
+  message: string
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) {
+  const store = getStore(`store-${CURRENT_WEEK}`);
+
+  try {
+    await store.setJSON("submissions", []);
+    res.status(200).json({ message: 'cleared the store' })
+  }
+  catch(e) {
+    console.log(e)
+  }
+}
