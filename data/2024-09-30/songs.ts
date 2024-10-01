001cb8c5e06d46f41fc437414c0e7294d8ca43f0
Anne LoVerso <anne.loverso@gmail.com>
2024-09-30 23:57:32 -0400

    add 9/30

---
+++ data/2024-09-30/songs.ts
@@ -0,0 +1,81 @@
+export default [
+  {
+    title: "MONEY ON THE DASH - SPED UP",
+    artist: "Elley Duhé, Whethan",
+    comment: "",
+  },
+  {
+    title: "Outside",
+    artist: "Kero Kero Bonito",
+    comment: "",
+  },
+  {
+    title: "Hidden In the Sand",
+    artist: "Tally Hall",
+    comment: "Love the old-timey sound, and I wish more of their songs sounded like this.",
+  },
+  {
+    title: "Intro (Like Velvet)",
+    artist: "Nao",
+    comment: "Like Velvet is the intro song to Nao's debut album, For All We Know, and I love that it sounds like velvet. It's short and sweet, and then just as Nao sings, \"it's gone,\" the song ends. It sets the right tone to draw me in and yearn for more, pulling me to listen to the rest of the album (which I gladly did).",
+  },
+  {
+    title: "my dance",
+    artist: "DADADA!, Itsuka, New K",
+    comment: "every song on this album is about a minute long and they all go crazy",
+  },
+  {
+    title: "Ambiguous Norway (instrumental)",
+    artist: "Cassandra Jenkins",
+    comment: "Movie ending\n" +
+      "=\n" +
+      "this song\n" +
+      "+\n" +
+      "character looking out of window of (bus | train | plane) contemplating the choice to leave (childhood home parents are moving out of | old friend with a different life now | potential love interest that did not work out) when they suddenly discover (handwritten note | kid's drawing) stuck in the pages of the (never-finished book from childhood bookshelf | book of poems drunkenly put in their jacket pocket) and they contentedly decide to (go back now | never go back).",
+  },
+  {
+    title: "jawbreaker",
+    artist: "mgk",
+    comment: "Say what you will about machine gun kelly...\n" +
+      "\n" +
+      "That’s it. say what you will",
+  },
+  {
+    title: "la luna enamorada",
+    artist: "Kali Uchis",
+    comment: "could not be longer yet leaves you wanting more",
+  },
+  {
+    title: "Rain",
+    artist: "Rob Scallon",
+    comment: " 2:02, please forgive me.\n" +
+      "\n" +
+      "A wild video is when Rob and 2 buddies try to recreate the delay and reverb in this song 100% acoustically:\n" +
+      "https://www.youtube.com/watch?v=MNzBFgwkU0A&t=3s",
+  },
+  {
+    title: "Mirror",
+    artist: "Grace Ives",
+    comment: "wish this song was 30seconds longer & had some sort of resolution, but it’s a catchy one",
+  },
+  {
+    title: "I Love Kanye",
+    artist: "Kanye West",
+    comment: "I miss the old kanye :/",
+  },
+  {
+    title: "i'm going to fail my class",
+    artist: "mage tears",
+    comment: "",
+  },
+  {
+    title: "Come June",
+    artist: "Yoshi Flower",
+    comment: "",
+  },
+  {
+    title: "Spiral",
+    artist: "Pinegrove",
+    comment: "a simple little self-care + love song that is meant to sound like that visual trope of the days of a calendar fluttering off in quick succession to show the passage of time. each sentence can be thought of as a different day. take care of yourselves and don't forget to hydrate folks!",
+  },
+];
--- src/domain/consts.ts
+++ src/domain/consts.ts
@@ -1,1 +1,1 @@
-export const CURRENT_WEEK = "2024-09-23";
+export const CURRENT_WEEK = "2024-09-30";
