9741dc5c02f1dffbd833cadc42e1a46d93a1ca81
Anne LoVerso <anne.loverso@gmail.com>
2024-11-08 16:57:33 -0500

    add answers

--- data/2024-11-04/answers.ts
+++ data/2024-11-04/answers.ts
@@ -1,3 +1,12 @@
 export default {
-
+  "Variation #9: Pantelleria 1.2": "Matt Luedke",
+  "bad guy": "Tristan Vanech",
+  "Orphaned Skies": "sanni",
+  "Easy Come Easy Go": "aelv13",
+  "Paper Planes": "Amani Farooque",
+  "Africa": "jasnoodle",
+  "Say You Will": "Naman Agrawal",
+  "Marble Machine": "John Chan",
+  "RIIVERDANCE": "Hames",
+  "Wind Coda": "Noah Marcus"
 }
