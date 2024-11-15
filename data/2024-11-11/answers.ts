88a5291a222cdea0eef2a6578270af6f1b76d2e8
Anne LoVerso <anne.loverso@gmail.com>
2024-11-15 18:39:45 -0500

    11/11 answers

--- data/2024-11-11/answers.ts
+++ data/2024-11-11/answers.ts
@@ -1,3 +1,12 @@
 export default {
-
+  "Rangers": "aelv13",
+  "Nuvole Bianche": "Hames",
+  "Dreamy Brown Noise": "Amani Farooque",
+  "pretty isn’t pretty": "Matt Luedke",
+  "Om Shanti Om": "sanni",
+  "Far From Any Road": "Naman Agrawal",
+  "The Chicken": "case451",
+  "Bellas Finals: Price Tag/Don’t You (Forget About Me)/Give Me Everything/Just The Way You Are/Party In The U.S.A./Turn The Beat Around": "Noah Marcus",
+  "Down To The Honkytonk": "Dan Hinze",
+  "Boy": "Tristan Vanech"
 }
