bef2f9839e8fa7511d0e997a112cbb5b81a91688
Anne LoVerso <anne.loverso@gmail.com>
2024-11-22 11:25:21 -0500

    11/18

---
+++ data/2024-11-18/songs.ts
@@ -0,0 +1,86 @@
+export default [
+  {
+    title: "Run Boy Run",
+    artist: "Woodkid",
+    comment: "imagining it like as part of my movie soundtrack during a dramatic action sequence",
+  }, {
+    title: "Crown On the Ground",
+    artist: "Sleigh Bells",
+    comment: "am I successfully pulling of a heist? am i wearing an awesome outfit and walking down the hallway? am I running really fast and passing everyone on the track? am I entering the function and everyone turns to look? i don't know, all I know is this is playing and I am the main character 💅",
+  },
+  {
+    title: "LET THE WORLD BURN",
+    artist: "Chris Grey",
+    comment: "",
+  },
+  {
+    title: "Red Swan",
+    artist: "Yoshiki, HYDE",
+    comment: "I believe this is the opening theme of Attack on Titan, S3 part 1. This is, of course, the extended version. It's always kind of a little odd, but building up to the end still hits hard.\n" +
+      "\n" +
+      "\"We'll see the end... we'll be the end...\"",
+  },
+  {
+    title: "COCOJOEY'S THEME",
+    artist: "Cocojoey",
+    comment: "Using this as an excuse to plug my friend's main musical project, which in this case also has strong OST/main character vibes.",
+  },
+  {
+    title: "Don't Rain On My Parade",
+    artist: "Barbra Streisand",
+    comment: "Doesn't get more main character than Streisand belting about unstoppable determination in a musical. Great for a 6 am alarm when you're trying to get psyched for a big life change",
+  },
+  {
+    title: "Oro Di Miele",
+    artist: "Bassolino",
+    comment: "If my life were an anime this would be my opening track.",
+  },
+  {
+    title: "Mayor Bloomberg (Giant Margarita)",
+    artist: "Awkwafina",
+    comment: "worrying about how policy changes might affect my ability to get messy is the good kind of MCE imo 🙏 🍹 good thing they didnt get banned",
+  },{
+    title: "Von dutch",
+    artist: "Charli xcx",
+    comment: "",
+  },{
+    title: "Champion",
+    artist: "Warpaint",
+    comment: "collective vs individual identity... i think that counts as main character",
+  },
+  {
+    title: "Heart Of Glass",
+    artist: "Blondie",
+    comment: "Disco beat gives that bee gees/ baby driver walking down the street to the beat vibe",
+  },
+  {
+    title: "You're The Best",
+    artist: "Joe \"Bean\" Esposito\n",
+    comment: "Could substitute any '80s training montage music here, but this is my favorite."
+  },
+  {
+    title: "Wolk",
+    artist: "Dans Dans",
+    comment: "walking down the street... but make it EPIC",
+  },
+  {
+    title: "YO VISTO ASÍ",
+    artist: "Bad Bunny",
+    comment: "",
+  },
+  {
+    title: "Good as Hell",
+    artist: "Lizzo",
+    comment: "This song gives me BIG main character energy. \"I do my hair toss / Check my nails / Baby how you feelin'? / Feeling good as hell\" I literally do this as part of my solo dance party! 😆😃 ",
+  },
+  {
+    title: "wyd - Live at Electric Lady",
+    artist: "Remi Wolf",
+    comment: "“I gotta bone to pick 😡”, this version feels even extra main-charactery ",
+  },
+  {
+    title: "Jordan Belfort",
+    artist: "Wes Walker, Dyl",
+    comment: "",
+  },
+];
--- src/domain/consts.ts
+++ src/domain/consts.ts
@@ -1,1 +1,1 @@
-export const CURRENT_WEEK = "2024-11-11";
+export const CURRENT_WEEK = "2024-11-18";
