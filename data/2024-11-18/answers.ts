009e2bccf663b5d9af2b23831a8373ebc687e09c
Anne LoVerso <anne.loverso@gmail.com>
2024-11-22 17:01:11 -0500

    11/18 answers

--- data/2024-11-18/answers.ts
+++ data/2024-11-18/answers.ts
@@ -1,3 +1,19 @@
 export default {
-
+  "Crown On the Ground": "Noah Marcus",
+  "Red Swan": "case451",
+  "COCOJOEY'S THEME": "Dan Hinze",
+  "Don't Rain On My Parade": "melanie.mazanec",
+  "Von dutch": "lizzy",
+  "Champion": "Matt Luedke",
+  "You're The Best": "Hames",
+  "Wolk": "bturndorf",
+  "Jordan Belfort": "Tristan Vanech",
+  "Mayor Bloomberg (Giant Margarita)": "jasnoodle",
+  "Run Boy Run": "aelv13",
+  "LET THE WORLD BURN": "Naman Agrawal",
+  "Oro Di Miele": "Oreofe Aderibigbe",
+  "wyd - Live at Electric Lady": "Amani Farooque",
+  "Heart Of Glass": "Walker Gosrich",
+  "YO VISTO ASÍ": "sanni",
+  "Good as Hell": "anjupoe"
 }
