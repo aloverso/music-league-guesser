ae49c0dad45cbd17958820da64e4248453efa346
Anne LoVerso <anne.loverso@gmail.com>
2025-01-24 13:16:21 -0500

    1/20 answers

--- data/2025-01-20/answers.ts
+++ data/2025-01-20/answers.ts
@@ -1,3 +1,15 @@
 export default {
-
+  "'39 - Remastered 2011": "aelv13",
+  "Shadows": "Hames",
+  "Kill All Your Friends": "case451",
+  "To Be Alone With You": "John Chan",
+  "How It Ends": "Oreofe Aderibigbe",
+  "I'm Satisfied": "James Viall",
+  "Watercolour Guitars": "Matt Luedke",
+  "It's No Game (Pt. 1) - 2017 Remaster": "Noah Marcus",
+  "Body And Soul": "bturndorf",
+  "Adore": "sanni",
+  "Florida Kilos": "jasnoodle",
+  "Be the One": "Amani Farooque",
+  "Motion": "Naman Agrawal"
 }
