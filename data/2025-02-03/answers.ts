74d4b07dda0c063a4bbf5bb2af34d680010fe527
Anne LoVerso <anne.loverso@gmail.com>
2025-02-07 14:51:56 -0500

    answers 2/3

--- data/2025-02-03/answers.ts
+++ data/2025-02-03/answers.ts
@@ -1,3 +1,19 @@
 export default {
-
+  "Atomic Garden": "aelv13",
+  "Killing In the Name": "James Viall",
+  "A Whole New World": "jasnoodle",
+  "Suck My Kiss": "Dan Hinze",
+  "No Ordinary Love": "Walker Gosrich",
+  "Walking on Broken Glass": "Matt Luedke",
+  "Rhythm Is a Dancer": "Hames",
+  "Aluá": "melanie.mazanec",
+  "Xtal": "Noah Marcus",
+  "Everybody Hurts": "Amani Farooque",
+  "Wake Up": "bturndorf",
+  "I Have Nothing": "Oreofe Aderibigbe",
+  "They Reminisce Over You (T.R.O.Y.)": "Naman Agrawal",
+  "Youth Against Fascism": "Marc S",
+  "Crying": "John Chan",
+  "Friday I'm in Love": "case451",
+  "Jericho": "sanni"
 }
