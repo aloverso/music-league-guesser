e166081fda516a0a60a9ee57349284b81b04f039
Anne LoVerso <anne.loverso@gmail.com>
2025-02-21 14:39:00 -0500

    results 2/17

--- data/2025-02-17/answers.ts
+++ data/2025-02-17/answers.ts
@@ -1,3 +1,12 @@
 export default {
-
+  "Spiderman": "case451",
+  "Good Riddance (Time of Your Life)": "aelv13",
+  "Zombie": "Matt Luedke",
+  "Cans and Brahms (Extracts from Brahms' 4th Symphony in E Minor, Third Movement) - 2008 Remaster": "melanie.mazanec",
+  "Eye in the Wall": "Noah Marcus",
+  "Live & Learn": "Naman Agrawal",
+  "Sadie Sorceress": "sanni",
+  "Lovefool": "jasnoodle",
+  "Helter Skelter - Remastered 2009": "Hames",
+  "We're Going to Be Friends": "Amani Farooque"
 }
